b25161e9c4636adfc20fcb1d99cc8e36e5e81d5e
Michael Carpenter <malcom2073@gmail.com>
2012-07-08 10:07:26 -0400

    Addition of copyright notices

--- src/tableview3d.cpp
+++ src/tableview3d.cpp
@@ -1,3 +1,21 @@
+/***************************************************************************
+*   Copyright (C) 2012  Michael Carpenter (malcom2073)                     *
+*                                                                          *
+*   This file is a part of EMStudio                                        *
+*                                                                          *
+*   EMStudio is free software: you can redistribute it and/or modify       *
+*   it under the terms of the GNU General Public License version 2 as      *
+*   published by the Free Software Foundation.                             *
+*                                                                          *
+*   EMStudio is distributed in the hope that it will be useful,            *
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
+*   GNU General Public License for more details.                           *
+									   *
+*   You should have received a copy of the GNU General Public License      *
+*   along with this program.  If not, see <http://www.gnu.org/licenses/>.  *
+****************************************************************************/
+
 #include "tableview3d.h"
 #include <QDebug>
 #include <QMessageBox>
