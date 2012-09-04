32e0b36a8a276436ae2ab79508693f660ef95a9f
Michael Carpenter <malcom2073@gmail.com>
2012-09-03 21:46:40 -0400

    Addition of -h and --help CLI options

--- src/main.cpp
+++ src/main.cpp
@@ -18,21 +18,78 @@
 #include <QtGui/QApplication>
 #include "mainwindow.h"
 #include <QString>
+
+QList<QPair<QString,QString> > getArgs(int argc, char **argv)
+{
+	bool nextisarg = false;
+	QString current = "";
+	QString currentarg;
+	QList<QPair<QString,QString> > retval;
+	for (int i=1;i<argc;i++)
+	{
+		QString arg = QString(argv[i]);
+		if (arg.startsWith("-") || arg.startsWith("--"))
+		{
+			if (nextisarg)
+			{
+				//qDebug() << "Param with no arg" << current << currentarg;
+				retval.append(QPair<QString,QString>(current,currentarg.trimmed()));
+				currentarg = "";
+			}
+			nextisarg = true;
+			current = arg;
+		}
+		else
+		{
+			if (nextisarg)
+			{
+				//qDebug() << "Param:" << current << "Arg:" << arg;
+				currentarg += arg + " ";
+			}
+			else
+			{
+				//Invalid
+				qDebug() << "Invalid arg";
+			}
+		}
+	}
+	if (nextisarg)
+	{
+		//qDebug() << "Param with no arg" << current << currentarg;
+		retval.append(QPair<QString,QString>(current,currentarg.trimmed()));
+	}
+	return retval;
+}
 int main(int argc, char *argv[])
 {
 	QApplication a(argc, argv);
 	QString port = "";
-	if (argc > 1)
+	QList<QPair<QString,QString> > args = getArgs(argc,argv);
+	for (int i=0;i<args.size();i++)
 	{
-		QString arg = QString(argv[1]);
-		if (arg.contains("="))
+		if (args[i].first == "--dev" || args[i].first == "-d")
 		{
-			if (arg.split("=")[0] == "--dev")
-			{
-				port = arg.split("=")[1];
-			}
+			port = args[i].second;
+		}
+		else if (args[i].first == "--help" || args[i].first == "-h")
+		{
+			qDebug() << "Help";
+			qDebug() << "Available Commands:";
+			qDebug() << "-d <dev>\t--dev <dev>\tOpen EMStudio, connecting to device <dev>";
+			qDebug() << "-h\t\t--help\t\tShow this help";
+			return 0;
+		}
+		else
+		{
+			qDebug() << "Unknown command" << args[i].first;
+			qDebug() << "Help";
+			qDebug() << "Available Commands:";
+			qDebug() << "-d <dev>\t--dev <dev>\tOpen EMStudio, connecting to device<dev>";
+			qDebug() << "-h\t\t--help\t\tShow this help";
+			return 0;
 		}
 	}
+
 	MainWindow w;
 	if (port != "")
 	{
