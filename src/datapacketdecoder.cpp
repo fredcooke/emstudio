f66d167026caae16973e94140542162ad01a5ffe
Fred Cooke <fred.cooke@gmail.com>
2012-10-13 14:53:06 +0200

    Fix a few wrong scalings and add/fix a few comments.

--- src/datapacketdecoder.cpp
+++ src/datapacketdecoder.cpp
@@ -45,6 +45,7 @@ void DataPacketDecoder::decodePayload(QByteArray payload)
 
 void DataPacketDecoder::populateDataFields()
 {
+	// CoreVars
 	m_dataFieldList.append(DataField("IAT","Intake Air Temperature",0,2,100,-273.15));
 	m_dataFieldList.append(DataField("CHT","Coolant/Head Temperature",2,2,100,-273.15));
 	m_dataFieldList.append(DataField("TPS","Throttle Position Sensor",4,2,640.0));
@@ -61,26 +62,32 @@ void DataPacketDecoder::populateDataFields()
 	m_dataFieldList.append(DataField("RPM","Engine Speed",26,2,2.0));
 	m_dataFieldList.append(DataField("DRPM","Delta RPM",28,2,1.0));
 	m_dataFieldList.append(DataField("DDRPM","Delta Delta RPM",30,2,1.0));
+
+	// DerivedVars
 	m_dataFieldList.append(DataField("LoadMain","Configurable Unit of Load",32,2,512.0));
 	m_dataFieldList.append(DataField("VEMain","Volumentric Efficiency",34,2,512.0));
 	m_dataFieldList.append(DataField("Lambda","Integral Lambda",36,2,32768.0));
 	m_dataFieldList.append(DataField("AirFlow","Raw Intermediate",38,2,1.0));
 	m_dataFieldList.append(DataField("densityAndFuel","raw Intermediate",40,2,1.0));
 	m_dataFieldList.append(DataField("BasePW","Raw PW Before corrections",42,2,1250));
-	m_dataFieldList.append(DataField("ETE","Engine Temp Enrichment Percentage",44,2,100.0/32768.0));
-	m_dataFieldList.append(DataField("TFCTotal","Total Transient Fuel Corrections",46,2,100.0/32768.0));
+	m_dataFieldList.append(DataField("ETE","Engine Temp Enrichment Percentage",44,2,100.0/16384.0));
+	m_dataFieldList.append(DataField("TFCTotal","Total Transient Fuel Corrections",46,2,1250)); // Needs to be signed short / int16
 	m_dataFieldList.append(DataField("EffectivePW","Actual PW of fuel delivery",48,2,1250.0));
 	m_dataFieldList.append(DataField("IDT","PW duration before fuel flow begins",50,2,1250.0));
 	m_dataFieldList.append(DataField("RefPW","Reference electrical PW",52,2,1250.0));
 	m_dataFieldList.append(DataField("Advance","Ignition advance",54,2,50.0));
-	m_dataFieldList.append(DataField("Dwell","Dwell period",56,2,1520.0));
+	m_dataFieldList.append(DataField("Dwell","Dwell period",56,2,1250.0));
+
+	// KeyUserDebug
 	m_dataFieldList.append(DataField("tempClock","Once per datalog message",58,1,1.0));
 	m_dataFieldList.append(DataField("spareChar","Unused at this time",59,1,1.0));
-	//m_dataFieldList.append(DataField("coreStatusA","Duplicate",60,1,1.0,0,true));
-	//m_dataFieldList.append(DataField("decoderFlags","Various decoder state flags",61,1,1.0,0,true));
-	//m_dataFieldList.append(DataField("flaggableFlags","Flags to go with flaggables",62,2,1.0,0,true));
 
-	//Flaggable flags
+	// TODO bits:
+	m_dataFieldList.append(DataField("coreStatusA","Duplicate",60,1,1.0,0,true)); // Needs flags
+	m_dataFieldList.append(DataField("decoderFlags","Various decoder state flags",61,1,1.0,0,true)); // Needs flags
+
+	//m_dataFieldList.append(DataField("flaggableFlags","Flags to go with flaggables",62,2,1.0,0,true)); Listed below per bit:
+	// Flaggable flags
 	m_dataFieldList.append(DataField("callsToUISRs","to ensure we aren't accidentally triggering unused ISRs.",62,2,1.0,0,0,0,true,0));
 	m_dataFieldList.append(DataField("lowVoltageConditions","low voltage conditions.",62,2,1.0,0,0,0,true,1));
 	m_dataFieldList.append(DataField("decoderSyncLosses","Number of times cam, crank or combustion sync is lost.",62,2,1.0,0,0,0,true,2));
@@ -89,7 +96,7 @@ void DataPacketDecoder::populateDataFields()
 	m_dataFieldList.append(DataField("serialNoiseErrors","Incremented when noise is detected",62,2,1.0,0,0,0,true,5));
 	m_dataFieldList.append(DataField("serialFramingErrors","Incremented when a framing error occurs",62,2,1.0,0,0,0,true,6));
 	m_dataFieldList.append(DataField("serialParityErrors","Incremented when a parity error occurs",62,2,1.0,0,0,0,true,7));
-	/*MISSING!*/m_dataFieldList.append(DataField("serialOverrunErrors","Incremented when overrun occurs (duplicated in KeyUserDebug below",62,2,1.0,0,0,0,true,8));
+	m_dataFieldList.append(DataField("serialOverrunErrors","Incremented when overrun occurs (count available in KeyUserDebug below",62,2,1.0,0,0,0,true,8));
 	m_dataFieldList.append(DataField("serialEscapePairMismatches","Incremented when an escape is found but not followed by an escapee",62,2,1.0,0,0,0,true,9));
 	m_dataFieldList.append(DataField("serialStartsInsideAPacket","Incremented when a start byte is found inside a packet",62,2,1.0,0,0,0,true,10));
 	m_dataFieldList.append(DataField("serialPacketsOverLength","Incremented when the buffer fills up before the end",62,2,1.0,0,0,0,true,11));
@@ -98,7 +105,6 @@ void DataPacketDecoder::populateDataFields()
 	m_dataFieldList.append(DataField("commsDebugMessagesNotSent","Incremented when a debug message can't be sent due to the TX buffer",62,2,1.0,0,0,0,true,14));
 	m_dataFieldList.append(DataField("commsErrorMessagesNotSent","Incremented when an error message can't be sent due to the TX buffer",62,2,1.0,0,0,0,true,15));
 
-
 	m_dataFieldList.append(DataField("currentEvent","Which input event was last",64,1,1.0));
 	m_dataFieldList.append(DataField("syncLostWithThisID","UID for reason beind loss of sync",65,1,1.0));
 	m_dataFieldList.append(DataField("syncLostOnThisEvent","Where in the input pattern sync was lost",66,1,1.0));
@@ -106,7 +112,7 @@ void DataPacketDecoder::populateDataFields()
 	m_dataFieldList.append(DataField("syncResetCalls","Sum of losses, corrections, and state clears",68,1,1.0));
 	m_dataFieldList.append(DataField("primaryTeethSeen","",69,1,1.0));
 	m_dataFieldList.append(DataField("secondaryTeethSeen","",70,1,1.0));
-	m_dataFieldList.append(DataField("serialOverrunErrorsCount","",71,1,1.0)); //These three have "Count" added to the end.
+	m_dataFieldList.append(DataField("serialOverrunErrorsCount","",71,1,1.0)); //These three have "Count" added to the end of the name to avoid conflict with flags above.
 	m_dataFieldList.append(DataField("serialHardwareErrorsCount","",72,1,1.0));
 	m_dataFieldList.append(DataField("serialAndCommsCodeErrorsCount","",73,1,1.0));
 	m_dataFieldList.append(DataField("inputEventTimeTolerance","",74,2,1.0));
@@ -118,10 +124,9 @@ void DataPacketDecoder::populateDataFields()
 	m_dataFieldList.append(DataField("zsp5","",86,2,1.0));
 	m_dataFieldList.append(DataField("zsp4","",88,2,1.0));
 	m_dataFieldList.append(DataField("zsp3","",90,2,1.0));
-	m_dataFieldList.append(DataField("clockInMilliSeconds","Clock in milliseconds",92,2,1.0));//was 88
+	m_dataFieldList.append(DataField("clockInMilliSeconds","Clock in milliseconds",92,2,1.0));
 	m_dataFieldList.append(DataField("clock8thMssInMillis","Clock in 8th milliseconds",94,2,8.0));
-	m_dataFieldList.append(DataField("ignitionLimiterFlags","",96,1,1.0));
-	m_dataFieldList.append(DataField("injectionLimiterFlags","",97,1,1.0));
-
+	m_dataFieldList.append(DataField("ignitionLimiterFlags","",96,1,1.0)); // Needs flags
+	m_dataFieldList.append(DataField("injectionLimiterFlags","",97,1,1.0)); // Needs flags
 
 }
