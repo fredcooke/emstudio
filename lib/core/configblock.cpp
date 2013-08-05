f6075c6014de5835d1dca18b710d4a3d91855f98
Michael Carpenter <malcom2073@gmail.com>
2013-08-05 08:56:52 -0400

    Missed some files

---
+++ lib/core/configblock.cpp
@@ -0,0 +1,37 @@
+/************************************************************************************
+ * EMStudio - Open Source ECU tuning software                                       *
+ * Copyright (C) 2013  Michael Carpenter (malcom2073@gmail.com)                     *
+ *                                                                                  *
+ * This file is a part of EMStudio                                                  *
+ *                                                                                  *
+ * EMStudio is free software; you can redistribute it and/or                        *
+ * modify it under the terms of the GNU Lesser General Public                       *
+ * License as published by the Free Software Foundation, version                    *
+ * 2.1 of the License.                                                              *
+ *                                                                                  *
+ * EMStudio is distributed in the hope that it will be useful,                      *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of                   *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU                *
+ * Lesser General Public License for more details.                                  *
+ *                                                                                  *
+ * You should have received a copy of the GNU Lesser General Public                 *
+ * License along with this program; if not, write to the Free Software              *
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA   *
+ ************************************************************************************/
+
+#include "configblock.h"
+
+ConfigBlock::ConfigBlock()
+{
+}
+ConfigBlock::ConfigBlock(QString name,QString type,QString override,unsigned short locationid, unsigned short size,unsigned short elementsize,unsigned short offset, QList<QPair<QString,double> > calc)
+{
+	m_name = name;
+	m_type = type;
+	m_sizeOverride = override;
+	m_locationId = locationid;
+	m_size = size;
+	m_elementSize = elementsize;
+	m_offset = offset;
+	m_calc = calc;
+}
